5b9c190e54e5600073174992728e764ea35aaa74
cehayford <sean.alpha18@gmail.com>
2025-10-22 12:57:30 -0500

    Deletion of all app implementation

--- voting/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-admin.site.register(Headline)
-admin.site.register(Poll_information)
-admin.site.register(Poll)
-admin.site.register(Option)
-admin.site.register(Vote)
