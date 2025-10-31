90f318e0e0aa691198682a890ff9707d0af07d98
cehayford <sean.alpha18@gmail.com>
2025-10-31 12:12:08 -0500

    feat: grouping sub_category and re-structuring the nominee view

--- nominee/views.py
+++ nominee/views.py
@@ -1,10 +1,12 @@
-from urllib import request
 from django.shortcuts import get_object_or_404, render
 from voting.models import *
+from itertools import groupby
+from urllib import request
 
 
 # Create your views here.
 def Nominee_view(request, headline_id):
     headline = get_object_or_404(Headline, pk=headline_id)
-    nominee = Poll_information.objects.filter(headline=headline).order_by('-creation_date') # Assume
-    return render(request, 'nominee/voting_centre.html', {'headline': headline, 'nominee': nominee})
+    nominee = Poll_information.objects.filter(headline=headline).order_by('sub_category')
+    grouped_nominees = groupby(nominee, key=lambda x: x.sub_category)
+    return render(request, 'nominee/voting_centre.html', {'headline': headline, 'nominee': nominee, 'grouped_nominees': grouped_nominees})
