a0f003f6f6700e3c89a28085298027d6876319bf
cehayford <sean.alpha18@gmail.com>
2025-11-22 10:39:34 -0600

    creating structure set

--- voting/forms.py
+++ voting/forms.py
@@ -5,7 +5,6 @@ from .models import *
 class HeadlineForm(forms.ModelForm):
     class Meta:
         model = Headline
-        # fields = ['title', 'subtitle', 'logo', 'header_img']
         exclude = ['creator', 'updated_date']
         
 
@@ -20,16 +19,3 @@ class PollForm(forms.ModelForm):
     class Meta:
         model = Poll
         fields = ['headline', 'poll_info']
-
-
-# class OptionForm(forms.ModelForm):
-#     class Meta:
-#         model = Option
-#         fields = ['poll', 'opttext']
-
-
-# class VoteForm(forms.ModelForm):
-#     class Meta:
-#         model = Vote
-#         fields = ['user', 'option', 'poll']
-
